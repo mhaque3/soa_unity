d40d21c67c7bf0c1478dc094c92ad68dd6582b00
twupy1 <philip.twu@jhuapl.edu>
2015-10-20 14:11:39 -0400

    Merged SoaIsotropicSensor.cs, SoaPointedSensor.cs, and SoaSensor.cs all into SoaSensor.cs.  Beamwidths are set appropriately in simcontrol.  Got rid of gimbaled flag in soaSensor.  Instead, the sensor itself determines whether it is gimbaled or not by checking to see if its associated actor is a balloon.
    Need to update prefabs with the SoaSensor.

--- SOA/Assets/Custom Scripts/SoaSensor.cs
+++ SOA/Assets/Custom Scripts/SoaSensor.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using soa;
 
 public class SoaSensor : MonoBehaviour 
 {
+    // Sensor attributes
+    public float beamwidthDeg;
+    public Vector3 boresightUnitVector;
+    
+    // Sensor internal bookkeeping
     public PerceptionModality[] modes;
     public SoaActor soaActor;
     public List<GameObject> possibleDetections;
-    //public float UpdateRate = 1f;
-	// Use this for initialization
+
 	void Start () 
     {
+        boresightUnitVector = new Vector3(0, -1, 0); // Pointed straight down by default
         soaActor = gameObject.GetComponentInParent<SoaActor>();
 	}
 
@@ -18,7 +24,48 @@ public class SoaSensor : MonoBehaviour
     {
 	}
 
-    public virtual bool CheckSensorFootprint(GameObject target) { return true; }
+    // Cone beam antenna pattern
+    public bool CheckSensorFootprint(GameObject target)
+    {
+        // Re-evaluate boresight if sensor is gimbaled (balloon)
+        if (soaActor.type == SoaActor.ActorType.BALLOON)
+        {
+            // Get the belief dictionary and SPOI Belief
+            SortedDictionary<int, Belief> spoiBeliefDictionary = soaActor.getBeliefDictionary()[soa.Belief.BeliefType.SPOI];
+            Belief belief;
+            if (spoiBeliefDictionary.TryGetValue(soaActor.unique_id, out belief))
+            {
+                // Extract SPOI (x,y,z) in sim coordinates [km]
+                Vector3 spoi_km = new Vector3(
+                    ((Belief_SPOI)belief).getPos_x(),
+                    ((Belief_SPOI)belief).getPos_y(),
+                    ((Belief_SPOI)belief).getPos_z());
+
+                // Update the sensor boresight vector
+                boresightUnitVector.x = spoi_km.x - transform.position.x / SimControl.KmToUnity; // [km]
+                boresightUnitVector.y = spoi_km.y - soaActor.simAltitude_km; // [km]
+                boresightUnitVector.z = spoi_km.z - transform.position.z / SimControl.KmToUnity; // [km]
+                boresightUnitVector.Normalize();
+            }
+        }
+
+        // Compute relative vector
+        SoaActor targetActor = target.GetComponent<SoaActor>();
+        Vector3 sensorToTargetUnitVector;
+        sensorToTargetUnitVector.x = (target.transform.position.x - transform.position.x) / SimControl.KmToUnity; // [km]
+        sensorToTargetUnitVector.y = targetActor.simAltitude_km - soaActor.simAltitude_km; // [km]
+        sensorToTargetUnitVector.z = (target.transform.position.z - transform.position.z) / SimControl.KmToUnity; // [km]
+        sensorToTargetUnitVector.Normalize();
+
+        // Compute target cone angle relative to boresight 
+        double coneAngleDeg = (180.0f / Mathf.PI) * Mathf.Acos(
+            sensorToTargetUnitVector.x * boresightUnitVector.x +
+            sensorToTargetUnitVector.y * boresightUnitVector.y +
+            sensorToTargetUnitVector.z * boresightUnitVector.z);
+
+        // Check if target is within beamwidthDeg/2 of boresight
+        return coneAngleDeg <= (beamwidthDeg / 2);
+    }
 
     public void CheckDetections(List<GameObject> targets)
     {
