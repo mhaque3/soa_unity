ddb9c20b4132b374f1dde66b7915eb7324343dbf
twupy1 <philip.twu@jhuapl.edu>
2015-11-30 11:08:20 -0500

    Added classes to read/write directed adjacency matrices as binaries.

---
+++ SOA/Assets/Custom Scripts/NetworkTopologyWriter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace soa
+{
+    class NetworkTopologyWriter
+    {
+        // Members
+        private bool newStream;
+        private int N;
+        private List<bool> bitQueue;
+        private bool[] bitBuffer;
+        private FileStream fileStream;
+        private BufferedStream bufferedStream;
+        private byte[] tempByte;
+
+        // Constructor
+        public NetworkTopologyWriter(string outputFilename)
+        {
+            // Save arguments
+            fileStream = new FileStream(outputFilename, FileMode.Create);
+            bufferedStream = new BufferedStream(fileStream);
+      
+            // Specify that it is a new stream
+            newStream = true;
+
+            // Initialize lists
+            bitQueue = new List<bool>();
+
+            // Initialize temp storage
+            bitBuffer = new bool[8];
+            tempByte = new byte[1];
+        }
+
+        public void Add(bool[,] adjMatrix)
+        {
+            // Save and also write out the size of adj matrix if new stream
+            lock (bitQueue)
+            {
+                if (newStream)
+                {
+                    // Save matrix size
+                    N = adjMatrix.GetLength(0);
+
+                    // Write to stream in network order
+                    bufferedStream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(N)), 0, 4);
+
+                    // No longer a new stream
+                    newStream = false;
+                }
+
+                // Convert adj matrix to byte array and add to queue
+                EnqueueBitArray(FlattenAdjMatrix(adjMatrix));
+
+                // Try to write contents to stream
+                Write();
+            }
+        }
+
+        // Tries to write what is in the bit array as bytes into stream
+        private void Write()
+        {
+            lock (bitQueue)
+            {
+                while (bitQueue.Count > 8)
+                {
+                    // Copy 8 bits over to buffer and remove from the queue
+                    bitQueue.CopyTo(0, bitBuffer, 0, 8);
+                    bitQueue.RemoveRange(0, 8);
+
+                    // Write a byte from bitBuffer
+                    WriteByte();
+                }
+            }
+        }
+
+        private void WriteByte()
+        {
+            // Write a single byte from bitBuffer to the bufferedStream
+            new BitArray(bitBuffer).CopyTo(tempByte, 0);
+            bufferedStream.WriteByte(tempByte[0]);
+        }
+
+        // Writes what is left, flushes, and closes
+        public void Close()
+        {
+            lock (bitQueue)
+            {
+                if (bitQueue.Count > 0)
+                {
+                    // Write what is left and pad with false
+                    for (int i = 0; i < bitQueue.Count; i++)
+                    {
+                        bitBuffer[i] = bitQueue[i];
+                    }
+                    for (int i = bitQueue.Count; i < 8; i++)
+                    {
+                        bitBuffer[i] = false;
+                    }
+
+                    // Write the last byte
+                    WriteByte();
+
+                    // Clear the bit queue
+                    bitQueue.Clear();
+                }
+
+                // Flush and close streams
+                try
+                {
+                    bufferedStream.Flush();
+                    bufferedStream.Close();
+                }
+                catch (Exception)
+                {
+                }
+                try
+                {
+                    fileStream.Flush();
+                    fileStream.Close();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        // Extracts contents of adj matrix and puts in bit array
+        private bool[] FlattenAdjMatrix(bool[,] adjMatrix)
+        {
+            // Flatten 2D array
+            bool[] flattened = new bool[N * N];
+            Buffer.BlockCopy(adjMatrix, 0, flattened, 0, N * N);
+ 
+            // Convert to bit array
+            return flattened;
+        }
+
+        // Thread safe enqueue to bit array
+        private void EnqueueBitArray(bool[] b)
+        {
+            lock(bitQueue)
+            {
+                bitQueue.AddRange(b);
+            }
+        }
+    }
+}
---
+++ SOA/Assets/Custom Scripts/Post Analysis/NetworkTopologyReader.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace soa
+{
+    class NetworkTopologyReader
+    {
+        // Members
+        private FileStream fileStream;
+        private int N;
+        private List<bool[,]> adjHistory;
+        private List<bool> bitQueue;
+
+        // Constructor
+        public NetworkTopologyReader(string inputFilename)
+        {
+            // Save arguments
+            fileStream = new FileStream(inputFilename, FileMode.Open);
+
+            // Create new list
+            adjHistory = new List<bool[,]>();
+            bitQueue = new List<bool>();
+
+            // Read and save the data
+            Read();
+        }
+
+        // Read method
+        private void Read()
+        {
+            // First read the size N
+            byte[] intBuffer = new byte[4];
+            fileStream.Read(intBuffer, 0, 4);
+            N = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(intBuffer,0));
+
+            // Allocate space for NxN
+            byte[] adjMatrixBuffer = new byte[N * N];
+
+            // Process each adjacency matrix
+            int bytesRequested = (int)(Math.Round(1000.0f * (N*N) / 8.0f));
+            byte[] byteBuffer = new byte[bytesRequested];
+            int bytesRead = bytesRequested;
+            bool eof = false;
+            while (!eof)
+            {
+                // Read bytes
+                bytesRead = fileStream.Read(byteBuffer, 0, bytesRequested);
+
+                // Convert bytes to bools and add to bitQueue
+                BitArray bits = new BitArray(byteBuffer);
+                for (int i = 0; i < bytesRead*8; i++)
+                {
+                    bitQueue.Add(bits[i]);
+                }
+
+                // Convert bits to adjacency matrices
+                while (bitQueue.Count >= N * N)
+                {
+                    // Form a temporary adjacency matrix
+                    bool[,] tempAdj = new bool[N, N];
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            tempAdj[i, j] = bitQueue[N * i + j];
+                        }
+                    }
+                    bitQueue.RemoveRange(0, N * N);
+
+                    // Add the adj matrix to history
+                    adjHistory.Add(tempAdj);
+                }
+
+                // Check to see if we've reached end of file
+                eof = (bytesRead != bytesRequested);
+            }
+
+            // Close the stream
+            try
+            {
+                fileStream.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        // Get number of vectices
+        public int GetN()
+        {
+            return N;
+        }
+
+        // Get history of adjacency matrices
+        public List<bool[,]> GetAdjacencyHistory()
+        {
+            // Make a deep copy without altering internal members
+            List<bool[,]> returnList = new List<bool[,]>();
+            for (int i = 0; i < adjHistory.Count; i++)
+            {
+                returnList.Add((bool[,])adjHistory[i].Clone());
+            }
+            return returnList;
+        }
+    }
+}
