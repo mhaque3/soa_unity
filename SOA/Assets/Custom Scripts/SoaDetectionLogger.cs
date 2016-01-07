c819d6afef2cb81bf0d5c3ff68f1b6e619943ebb
Mike Biggins <michael.biggins@jhuapl.edu>
2016-01-07 14:18:23 -0500

    Added the two loggers

---
+++ SOA/Assets/Custom Scripts/SoaDetectionLogger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+public class SoaDetectionLogger {
+
+    private BinaryWriter writer;
+
+    public SoaDetectionLogger(string outputFilename)
+    {
+        writer = new BinaryWriter(File.Open(outputFilename, FileMode.Create));
+        
+    }
+
+    public void logDetection(UInt64 time, int detectorId, int detectedId)
+    {
+        writer.Write(time);
+        writer.Write(detectorId);
+        writer.Write(detectedId);
+    }
+
+
+    private static ReadDetectionLog logReader = null;
+
+    public static void openLogFile(String logFileName)
+    {
+        logReader = new ReadDetectionLog(logFileName);
+    }
+
+    public static bool readDetectionLog(out UInt64 time, out int detectorId, out int detectedId)
+    {
+        if (logReader != null)
+        {
+            return logReader.readDetection(out time, out detectorId, out detectedId);
+        }
+
+        time = 0;
+        detectedId = 0;
+        detectorId = 0;
+        return false;
+    }
+
+    private class ReadDetectionLog
+    {
+        BinaryReader reader;
+        public ReadDetectionLog(string logFileName)
+        {
+            reader = new BinaryReader(File.Open(logFileName, FileMode.Open));
+        }
+
+        public bool readDetection(out UInt64 time, out int detectorId, out int detectedId)
+        {
+            
+            try{
+                time = reader.ReadUInt64();
+                detectorId = reader.ReadInt32();
+                detectedId = reader.ReadInt32();
+            }catch(Exception e)
+            {
+                time = 0;
+                detectorId = 0;
+                detectedId = 0;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SOA/Assets/Custom Scripts/SoaGridCellMoveLogger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+public class SoaGridCellMoveLogger {
+
+    private BinaryWriter writer;
+
+    public SoaGridCellMoveLogger(string outputFilename)
+    {
+        writer = new BinaryWriter(File.Open(outputFilename, FileMode.Create));
+    }
+
+    public void logDetection(UInt64 time, int gridU, int gridV)
+    {
+        writer.Write(time);
+        writer.Write(gridU);
+        writer.Write(gridV);
+    }
+
+    private static ReadGridCellMoveLog logReader = null;
+
+    public static void openLogFile(String logFileName)
+    {
+        logReader = new ReadGridCellMoveLog(logFileName);
+    }
+
+    public static bool readGridCellMoveLog(out UInt64 time, out int gridU, out int gridV)
+    {
+        if (logReader != null)
+        {
+            return logReader.readGridCellMove(out time, out gridU, out gridV);
+        }
+
+        time = 0;
+        gridU = 0;
+        gridV = 0;
+        return false;
+    }
+
+    private class ReadGridCellMoveLog
+    {
+        BinaryReader reader;
+        public ReadGridCellMoveLog(string logFileName)
+        {
+            reader = new BinaryReader(File.Open(logFileName, FileMode.Open));
+        }
+
+        public bool readGridCellMove(out UInt64 time, out int gridU, out int gridV)
+        {
+
+            try
+            {
+                time = reader.ReadUInt64();
+                gridU = reader.ReadInt32();
+                gridV = reader.ReadInt32();
+            }
+            catch (Exception e)
+            {
+                time = 0;
+                gridU = 0;
+                gridV = 0;
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
