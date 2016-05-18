a4f83e31c3e4127ae88c98a16b107fd652a0f446
bobchalmers <robert.chalmers@jhuapl.edu>
2016-05-18 15:47:36 -0400

    Adding missing waypoint path file.

---
+++ SOA/Assets/Custom Scripts/Belief_WaypointPath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace soa
+{
+    class Waypoint
+    {
+        public float x, y, z, heading;
+        public bool visited;
+    }
+
+	class Belief_WaypointPath : Belief
+	{
+
+        private List<Waypoint> waypoints;
+        private ulong requestTime;
+
+        public Belief_WaypointPath(ulong requestTime, int id, List<Waypoint> path)
+            : base(id)
+        {
+            this.requestTime = requestTime;
+            this.waypoints = new List<Waypoint>(path); 
+        }
+
+        public override BeliefType getBeliefType()
+        {
+            return BeliefType.WAYPOINT_PATH;
+        }
+
+        public List<Waypoint> getWaypoints()
+        {
+            return new List<Waypoint>(waypoints);
+        }
+
+        public ulong getRequestTime()
+        {
+            return requestTime;
+        }
+	}
+}
